418a1e770ac21e958dce93c93674ae2b9a88342d
João Paulo <58531784+jp-grilo@users.noreply.github.com>
2022-04-01 18:10:25 -0300

    Add files via upload

--- Hospital/src/hospital/Hospital.java
+++ Hospital/src/hospital/Hospital.java
@@ -8,13 +8,20 @@ public class Hospital {
     public static void main(String[] args) {
 
         try{
-            BufferedReader br = new BufferedReader(
+            //Leitor do arquivo contendo os pacientes
+            BufferedReader brp = new BufferedReader(
                 new InputStreamReader(
-                    new FileInputStream("C:\\Users\\joaop\\OneDrive\\Documentos\\NetBeansProjects\\Hospital\\src\\Entrada.txt")));
+                    new FileInputStream("C:\\Users\\joaop\\OneDrive\\Documentos\\NetBeansProjects\\Hospital\\src\\Pacientes.txt")));
+            //Leitor do arquivo contendo os médicos
+            BufferedReader brm = new BufferedReader(
+                new InputStreamReader(
+                    new FileInputStream("C:\\Users\\joaop\\OneDrive\\Documentos\\NetBeansProjects\\Hospital\\src\\Medicos.txt")));
             
-            BufferedWriter bw = new BufferedWriter(
+            /*BufferedWriter bw = new BufferedWriter(
                 new OutputStreamWriter(
-                    new FileOutputStream("C:\\Users\\joaop\\OneDrive\\Documentos\\NetBeansProjects\\Hospital\\src\\Saida")));
+                    new FileOutputStream("C:\\Users\\joaop\\OneDrive\\Documentos\\NetBeansProjects\\Hospital\\src\\Saida")));*/
+            /*bw.close();*/
+            
             
             int count=0;
             String s;
@@ -25,7 +32,7 @@ public class Hospital {
             List idades = new ArrayList<>(); //lista das idades dos pacientes a serem atendidos no hospital
             List gravidades = new ArrayList<>(); //lista  das gracidades dos acidentes dos pacientes a serem atendidos no hospital
             
-            while((s = br.readLine())!=null){
+            while((s = brp.readLine())!=null){
                 array = s.split(", ");
                 for(String current : array){
                     
@@ -57,14 +64,57 @@ public class Hospital {
                 }   
             }
             
-            Paciente p0 = new Paciente((String)nomes.get(0), (String)cpfs.get(0), Integer.parseInt((String) idades.get(0)), Integer.parseInt((String)gravidades.get(0)));
-            Paciente p1 = new Paciente((String)nomes.get(1), (String)cpfs.get(1), Integer.parseInt((String) idades.get(1)), Integer.parseInt((String)gravidades.get(1)));
+            Pacientes p0 = new Pacientes((String)nomes.get(0), (String)cpfs.get(0), Integer.parseInt((String) idades.get(0)), Integer.parseInt((String)gravidades.get(0)));
+            Pacientes p1 = new Pacientes((String)nomes.get(1), (String)cpfs.get(1), Integer.parseInt((String) idades.get(1)), Integer.parseInt((String)gravidades.get(1)));
+            
+            
+            nomes.clear();
+            cpfs.clear();
+            idades.clear();
+            List tempoAtendimento = new ArrayList<>();
+            
+            while((s = brm.readLine())!=null){
+                array = s.split(", ");
+                for(String current : array){
+                    
+                    switch (count) {
+                        case 0:
+                            //nome
+                            nomes.add(current);
+                            count=1;
+                            break;
+                            
+                        case 1:
+                            //cpf
+                            cpfs.add(current);
+                            count=2;
+                            break;
+                            
+                        case 2:
+                            //idade
+                            idades.add(current);
+                            count=3;
+                            break;
+                            
+                        default:
+                            //Tempo que o médico leva para atender seus pacientes, calculado em minutos
+                            tempoAtendimento.add(current);
+                            count=0;
+                            break;
+                    }
+                }   
+            }
+            
+            Medicos m0 = new Medicos((String)nomes.get(0), (String)cpfs.get(0), Integer.parseInt((String) idades.get(0)), Integer.parseInt((String) tempoAtendimento.get(0)));
+            
             
             System.out.println(p0);
             System.out.println(p1);
+            System.out.println(m0);
             
-            br.close();
-            bw.close();
+            
+            brp.close();
+            brm.close();
         }catch(IOException ex){
             ex.printStackTrace();
         }  
---
+++ Hospital/src/hospital/Pessoas.java
@@ -0,0 +1,37 @@
+package hospital;
+
+public abstract class Pessoas {
+    protected String nome;
+    protected String cpf;
+    protected int idade;
+
+    public Pessoas(String nome, String cpf, int idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
