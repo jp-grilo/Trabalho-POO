99853897c6ccdd2f7b7c39ef9e9ed9c5e2ba3b30
João Paulo <58531784+jp-grilo@users.noreply.github.com>
2022-04-09 11:01:33 -0300

    Delete Hospital/src/hospital directory

--- Hospital/src/hospital/Procedimentos.java
+++
@@ -1,9 +0,0 @@
-package hospital;
-
-import java.util.*;
-
-public interface Procedimentos {
-    boolean logar(String username, String password);
-    void lerPacienteCadastro(ArrayList listaCadastro, Pacientes paciente);
-    void lerListaCadastro(ArrayList listaCadastro);
-}
